641ec28d6677311a90b25fa20f3c42021fbe6b9f
benpaodewoniu <1506316219@qq.com>
2021-05-17 15:48:13 +0800

    update

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-import requests
 import json
+import smtplib
+import sys
+from email.mime.text import MIMEText
+
+import requests
 
 url_tokens = 'https://api.zks.app/1/tokens'
 url_price = 'https://api.zks.app/1/tokens/price'
+
+From = sys.argv[1]
+PW = sys.argv[2]
+To = sys.argv[3]
+s_price = float(sys.argv[4])
+b_price = float(sys.argv[5])
 
 
 def get_token():
@@ -11,10 +21,6 @@ def get_token():
 
 
 def get_price(result):
-    # id_symbols = {}
-    # # id 对应 symbol
-    # for r in result:
-    #     id_symbols[r['id']] = r['symbol']
     id = 1
     for r in result:
         if r['symbol'] == "ZKS":
@@ -25,13 +31,20 @@ def get_price(result):
             return d['price']
 
 
-def saveText(path, price):
-    with open(path, 'w', encoding="utf-8") as f:
-        f.writelines(price)
+def sendEmail(price):
+    if float(price) < s_price or float(price) > b_price:
+        smtpObj = smtplib.SMTP_SSL('smtp.163.com', 465)
+        # 登录 SMTP 服务器
+        smtpObj.login(From, PW)
+        # 发送邮件
+        message = MIMEText(price, 'plain', 'utf-8')
+        message['From'] = From
+        message['To'] = To
+        message['Subject'] = "zks 价格"
+        smtpObj.sendmail(From, To, message.as_string())
 
 
 if __name__ == '__main__':
     result = get_token()
     price = get_price(result)
-    email_path = "email.txt"
-    saveText(email_path, price)
+    sendEmail(price)
--- main.sh
+++ main.sh
@@ -1,1 +1,4 @@
-python ./main.py
+set -eux
+sprice="1.5"
+bprice="1.8"
+python ./main.py ${{secrets.MAIL_USERNAME}} ${{secrets.MAIL_PASSWORD}} ${{secrets.RECEIVER}} $sprice $bprice
