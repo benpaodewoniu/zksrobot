dbe245491a66b3138b6b31aee3ea5d27c0b55bcd
benpaodewoniu <1506316219@qq.com>
2021-05-17 15:52:27 +0800

    update

--- main.sh
+++ main.sh
@@ -1,4 +1,4 @@
 set -eux
 sprice="1.5"
 bprice="1.8"
-python ./main.py ${{secrets.MAIL_USERNAME}} ${{secrets.MAIL_PASSWORD}} ${{secrets.RECEIVER}} $sprice $bprice
+python ./main.py ${{ secrets.MAIL_USERNAME }} ${{ secrets.MAIL_PASSWORD }} ${{ secrets.RECEIVER }} $sprice $bprice
